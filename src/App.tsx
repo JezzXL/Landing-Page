cc495f6054985d41ba6fe31d291f9143c44c1ca3
unknown <kionegamesyotuber337@gmail.com>
2025-12-03 23:52:21 -0300

    feat: integra HouseShowcase no App principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Hero from './components/Hero'
 import Stats from './components/Stats'
+import HouseShowcase from './components/HouseShowcase/HouseShowcase'
 import Projects from './components/Projects'
 import Features from './components/Features'
 import Contact from './components/Contact'
@@ -18,6 +19,10 @@ export default function App() {
     <div className="bg-black text-white">
       <Hero scrollY={scrollY} />
       <Stats />
+      
+      {/* Nova seção com modelo 3D da casa */}
+      <HouseShowcase />
+      
       <Projects />
       <Features />
       <Contact />
